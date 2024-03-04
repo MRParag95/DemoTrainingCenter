c830cec9d312937e6f6433c060ea74b7800673f4
MRParag95 <rahman.parag.95@gmail.com>
2024-03-04 09:50:26 +0600

    Added Base Director Model.

---
+++ src/main/java/com/mendax47/springboot/projects/trainingcenter/models/Director.java
@@ -0,0 +1,25 @@
+package com.mendax47.springboot.projects.trainingcenter.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Director extends Employee {
+    @Id
+    @SequenceGenerator(
+            name = "director_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "director_id_sequence"
+    )
+    private Long id;
+}
