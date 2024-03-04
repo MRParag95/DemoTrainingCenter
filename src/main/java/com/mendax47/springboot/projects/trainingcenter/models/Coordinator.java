f92b58e2d9e9ce692f6c847af75b887e9752f32a
MRParag95 <rahman.parag.95@gmail.com>
2024-03-04 10:02:34 +0600

    Added Base Coordinator Model.

---
+++ src/main/java/com/mendax47/springboot/projects/trainingcenter/models/Coordinator.java
@@ -0,0 +1,25 @@
+package com.mendax47.springboot.projects.trainingcenter.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Coordinator extends Employee {
+    @Id
+    @SequenceGenerator(
+            name = "coordinator_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "coordinator_id_sequence"
+    )
+    private Long id;
+}
