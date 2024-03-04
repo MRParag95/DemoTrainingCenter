c0924705d522ae9a3b34bf272047ca9a474c3c33
MRParag95 <rahman.parag.95@gmail.com>
2024-03-04 13:42:31 +0600

    Added Trainee Repository and Added Missing Annotations.

--- src/main/java/com/mendax47/springboot/projects/trainingcenter/repository/TraineeRepository.java
+++ src/main/java/com/mendax47/springboot/projects/trainingcenter/repository/TraineeRepository.java
@@ -1,10 +1,10 @@
 package com.mendax47.springboot.projects.trainingcenter.repository;
 
-import com.mendax47.springboot.projects.trainingcenter.models.Director;
+import com.mendax47.springboot.projects.trainingcenter.models.Trainee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DirectorRepository extends JpaRepository<Director, Long>, JpaSpecificationExecutor<Director> {
+public interface TraineeRepository extends JpaRepository<Trainee, Long>, JpaSpecificationExecutor<Trainee> {
 }
