0ba3acb33d49c505490a913ef8dc82d602655a73
MRParag95 <rahman.parag.95@gmail.com>
2024-03-04 10:09:15 +0600

    Added Course Base Model.

---
+++ src/main/java/com/mendax47/springboot/projects/trainingcenter/models/Course.java
@@ -0,0 +1,33 @@
+package com.mendax47.springboot.projects.trainingcenter.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "trainer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "trainer_id_sequence"
+    )
+    private Long id;
+    @Column(
+            nullable = false
+    )
+    private String name;
+    @Column(
+            nullable = false
+    )
+    private int duration;
+}
