a93593ee89e88dec9ff30b45acb460db4b731f46
MRParag95 <rahman.parag.95@gmail.com>
2024-03-05 13:35:48 +0600

    Cleaned Up Mess.

--- src/main/java/com/mendax47/springboot/projects/trainingcenter/repository/AbstractRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mendax47.springboot.projects.trainingcenter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AbstractRepository<T, V> extends JpaRepository<T, V> {
-}
