9231ea8d9c04466c0c1a6db729c3c4eae2c66c19
MRParag95 <rahman.parag.95@gmail.com>
2024-03-06 15:18:21 +0600

    Added Simple Global Handler.

---
+++ src/main/java/com/mendax47/springboot/projects/trainingcenter/errorhandlers/GlobalErrorHandler.java
@@ -0,0 +1,9 @@
+package com.mendax47.springboot.projects.trainingcenter.errorhandlers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@AllArgsConstructor
+public class GlobalErrorHandler {
+}
